954ab05819eb9a7edcec271cc99a42d37082eb9c
ABHISHEK MISHRA <51957554+Abhishekmishra-17@users.noreply.github.com>
2021-09-08 22:38:25 +0530

    Delete an.py

--- an.py
+++
@@ -1,6 +0,0 @@
-n=int(input())
-arr=list(map(int,input().split()))
-if(((arr[3]-arr[1])/(arr[2]-arr[0]))==((arr[5]-arr[1])/(arr[4]-arr[0]))):
-    print(str(int((arr[3]-arr[1])/(arr[2]-arr[0])))+"x-"+str(int((arr[3]-arr[1])/(arr[2]-arr[0])))+"y=0")
-else:
-    print("0")
