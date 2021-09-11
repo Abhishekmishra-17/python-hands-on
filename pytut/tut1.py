a9e0a93264e7f470b029faef901354f7c3c92fc9
ABHISHEK MISHRA <51957554+Abhishekmishra-17@users.noreply.github.com>
2021-09-11 22:53:34 +0530

    Add files via upload

---
+++ pytut/tut1.py
@@ -0,0 +1,35 @@
+# c
+#include<stdio.h>
+#include<conio.h>
+
+'''
+int a;
+int b;
+a=4;
+b=5;
+printf("%d",a+b);
+'''
+
+
+'''a=5#int
+b=6.7#float
+print(float(a)+int(b))
+'''
+
+#print("Hello word")
+#scanf("%d",&a);
+### arthi####
+a=int(input("enter the 1st number"))
+b=int(input("enter the 2nd number"))
+print("The sum of",a,"and",b,"is",a+b)
+print("The substraction of",a,"and",b,"is",a-b)
+print("The mul of",a,"and",b,"is",a*b)
+if(b<0):
+    print("Second number is negative")
+elif(b!=0):
+    print("The div1 of",a,"and",b,"is",a/b)#floating point division
+    print("The div2 of",a,"and",b,"is",a//b)#integer type division
+
+else:
+    print("division is not possible")
+#make a python program to find the sum,mul,division,power,and, or, of two numbers
